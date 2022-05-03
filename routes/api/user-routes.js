2e9b922f1b92bf75b44120b41ac598a7eda3e9b4
ravnishgupta <ravnish@gmail.com>
2022-05-02 22:23:31 -0700

    hashing passwords when creating and updating users

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
   // Access our User model and run .findAll() method)
-  User.findAll()
+  User.findAll({
+    attributes: { exclude: ['password'] },
+  })
   .then(dbUserData => res.json(dbUserData))
   .catch(err => {
     console.log(err);
@@ -15,9 +17,11 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
           id: req.params.id
         }
+        
     })
     .then(dbUserData => {
         if (!dbUserData) {
@@ -54,6 +58,7 @@ router.put('/:id', (req, res) => {
 
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
